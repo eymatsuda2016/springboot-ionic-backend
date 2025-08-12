2250d0853e11d4c8f535183467426921d947e738
eymatsuda2016 <eymatsuda2016@outlook.com>
2025-08-11 23:46:39 -0300

    Criado Repository e Service para Categoria

---
+++ src/main/java/com/eymatsuda/cursomc/services/CategoriaService.java
@@ -0,0 +1,21 @@
+package com.eymatsuda.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.eymatsuda.cursomc.domain.Categoria;
+import com.eymatsuda.cursomc.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+
+	@Autowired
+	private CategoriaRepository repo;
+	
+	public Categoria find(Integer id) {
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElse(null);
+	}
+}
