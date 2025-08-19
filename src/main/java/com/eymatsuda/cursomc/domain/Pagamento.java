6786d91b50ef625eaf3775066d145491cf00bb61
eymatsuda2016 <eymatsuda2016@outlook.com>
2025-08-19 14:37:18 -0300

    Atualização Utilizando somente gitIgnore

--- src/main/java/com/eymatsuda/cursomc/domain/Pagamento.java
+++ src/main/java/com/eymatsuda/cursomc/domain/Pagamento.java
@@ -13,6 +13,7 @@ import javax.persistence.OneToOne;
 
 import com.eymatsuda.cursomc.domain.enums.EstadoPagamento;
 import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 @Inheritance(strategy = InheritanceType.JOINED)
@@ -23,7 +24,7 @@ public abstract class Pagamento implements Serializable {
 	private Integer id;
 	private Integer estado;
 	
-	@JsonBackReference
+	@JsonIgnore
 	@OneToOne
 	@JoinColumn(name="pedido_id")
 	@MapsId
