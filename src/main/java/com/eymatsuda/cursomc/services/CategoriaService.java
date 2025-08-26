4236769328ba8a365957057effdd36e92af4a823
eymatsuda2016 <eymatsuda2016@outlook.com>
2025-08-25 21:13:04 -0300

    Deletando uma Categoria com DELETE

--- src/main/java/com/eymatsuda/cursomc/services/CategoriaService.java
+++ src/main/java/com/eymatsuda/cursomc/services/CategoriaService.java
@@ -3,10 +3,12 @@ package com.eymatsuda.cursomc.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.eymatsuda.cursomc.domain.Categoria;
 import com.eymatsuda.cursomc.repositories.CategoriaRepository;
+import com.eymatsuda.cursomc.services.exceptions.DataIntegrityException;
 import com.eymatsuda.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -29,6 +31,17 @@ public class CategoriaService {
 	public Categoria update(Categoria obj) {
 		find(obj.getId());
 		return repo.save(obj);
+	}
+
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+		}
+		catch(DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir uma categoria que possua produtos!");
+		}
+		
 	}
 }
 
