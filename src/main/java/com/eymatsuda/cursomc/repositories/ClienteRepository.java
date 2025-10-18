d71ec627d4b20843f8f7f5b72ae3309458430f3d
eymatsuda2016 <eymatsuda2016@outlook.com>
2025-10-18 12:55:57 -0300

    Validação Customizada: Email não Repetida na Inserção do Cliente

--- src/main/java/com/eymatsuda/cursomc/repositories/ClienteRepository.java
+++ src/main/java/com/eymatsuda/cursomc/repositories/ClienteRepository.java
@@ -2,10 +2,17 @@ package com.eymatsuda.cursomc.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.eymatsuda.cursomc.domain.Cliente;
+import java.util.List;
+
+
 
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Integer>{
+	
+	@Transactional(readOnly=true)
+	Cliente findByEmail(String email);
 
 }
